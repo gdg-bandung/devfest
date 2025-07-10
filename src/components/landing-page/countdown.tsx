e538a7bbcd92ff9a9ef23b64fca6833bd6bb4665
fikrialwan <fikriar93@gmail.com>
2025-07-10 23:02:38 +0700

    feat: add logic countdown

--- src/components/landing-page/countdown.tsx
+++ src/components/landing-page/countdown.tsx
@@ -1,22 +1,62 @@
+import { createSignal, createEffect } from "solid-js";
+
+const targetDate = new Date("2025-11-27T07:00:00+07:00");
+
 export default function Countdown() {
+  const [timeLeft, setTimeLeft] = createSignal({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  });
+
+  let interval: NodeJS.Timeout;
+
+  const updateCountdown = () => {
+    const now = new Date();
+    const diff = (targetDate.getTime() - now.getTime()) as number;
+    if (diff <= 0) {
+      setTimeLeft({ days: 0, hours: 0, minutes: 0, seconds: 0 });
+      if (interval) {
+        clearInterval(interval);
+      }
+      return;
+    }
+
+    const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+    setTimeLeft({ days, hours, minutes, seconds });
+  };
+
+  createEffect(() => {
+    interval = setInterval(updateCountdown, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  });
+
   return (
     <div class="flex items-center justify-between lg:justify-center lg:gap-12">
-      <CountdownItem label="Days" value={143} />
+      <CountdownItem label="Days" value={timeLeft().days} />
       <p class="text-3xl font-bold text-white">:</p>
-      <CountdownItem label="Hours" value={143} />
+      <CountdownItem label="Hours" value={timeLeft().hours} />
       <p class="text-3xl font-bold text-white">:</p>
-      <CountdownItem label="Minutes" value={143} />
+      <CountdownItem label="Minutes" value={timeLeft().minutes} />
       <p class="text-3xl font-bold text-white">:</p>
-      <CountdownItem label="Seconds" value={143} />
+      <CountdownItem label="Seconds" value={timeLeft().seconds} />
     </div>
   );
 }
 
-function CountdownItem({ label, value }: { label: string; value: number }) {
+function CountdownItem(props: { label: string; value: number }) {
   return (
-    <div class="py-1.5 px-3 flex flex-col justify-center items-center gap-[2px] md:gap-1 md:w-[108px] bg-dark-gray rounded-xl md:rounded-2xl">
-      <p class="text-lg md:text-2xl font-medium">{value}</p>
-      <p class="text-[10px] md:text-sm">{label}</p>
+    <div class="py-1.5 px-3 md:py-3 flex flex-col justify-center items-center gap-[2px] md:gap-1 md:w-[108px] bg-dark-gray rounded-xl md:rounded-2xl">
+      <p class="text-lg md:text-2xl font-medium">{props.value}</p>
+      <p class="text-[10px] md:text-sm">{props.label}</p>
     </div>
   );
 }
