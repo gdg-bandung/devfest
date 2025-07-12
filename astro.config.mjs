ecbac5b88abe0bd85f8b475eeaff39d53e99e84c
fikrialwan <fikriar93@gmail.com>
2025-07-12 11:48:25 +0700

    chore: setup cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,16 @@ import tailwindcss from "@tailwindcss/vite";
 
 import solidJs from '@astrojs/solid-js';
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://devfest.gdgbandung.com",
   integrations: [solidJs()],
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: cloudflare(),
 });
