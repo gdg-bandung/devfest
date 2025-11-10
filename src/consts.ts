64c3ec087ccf1ae85059c22a03db78f1246c7768
fikrialwan <fikriar93@gmail.com>
2025-11-10 23:07:00 +0700

    feat: update metatag

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "DevFest Bandung";
+export const SITE_TITLE = "DevFest Bandung 2025";
 export const SITE_DESCRIPTION =
   "Join us for the biggest developer conference in Southeast Asia. Learn, network, and grow with the tech community.";
 
