40407e90de908bfd29c1b7d2fb746fcb38f8bb99
fikrialwan <fikriar93@gmail.com>
2025-11-12 21:18:03 +0700

    feat(landing-page): add sponsor section

---
+++ src/data/sponsor.data.ts
@@ -0,0 +1,59 @@
+export const DIAMOND_SPONSORS = [
+    {
+        name: "Google for Developers",
+        image: "/images/sponsor-&-partner/google-for-developers.webp",
+    },
+    {
+        name: "Logitech",
+        image: "/images/sponsor-&-partner/logitech.webp",
+    },
+    {
+        name: "MSI",
+        image: "/images/sponsor-&-partner/msi.webp",
+        class: "py-[21px] md:py-[36px] lg:py-[56px]",
+    }
+]
+
+export const PLATINUM_SPONSORS = [
+    {
+        name: "WPU Course",
+        image: "/images/sponsor-&-partner/wpu-course.webp",
+    },
+    {
+        name: "Kliktron",
+        image: "/images/sponsor-&-partner/kliktron.webp",
+    },
+]
+
+export const SILVER_SPONSORS = [
+    {
+        name: "Cuci Wangi",
+        image: "/images/sponsor-&-partner/cuci-wangi.webp",
+    },
+    {
+        name: "Sarae Bakery",
+        image: "/images/sponsor-&-partner/sarae-bakery.webp",
+    },
+    {
+        name: "Lofi Coffee & Space",
+        image: "/images/sponsor-&-partner/lo-fi.webp",
+    },{
+        name: "heviitro",
+        image: "/images/sponsor-&-partner/heviitro.webp",
+    }
+]
+
+export const MEDIA_PARTNERS = [
+    {
+        name: "Radar Bandung",
+        image: "/images/sponsor-&-partner/radar-bandung.webp",
+    },
+    {
+        name: "bradio",
+        image: "/images/sponsor-&-partner/bradio.webp",
+        class: "py-[13px] md:py-4 lg:py-[18px]"
+    },{
+        name: "naramakna.id",
+        image: "/images/sponsor-&-partner/naramakna.webp",
+    }
+]
