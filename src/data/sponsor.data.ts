5c0dd69b2dc2381e68d6504663c6f9f670d6fc08
fikrialwan <fikriar93@gmail.com>
2025-11-19 10:53:27 +0700

    feat: update partner and add all link

--- src/data/sponsor.data.ts
+++ src/data/sponsor.data.ts
@@ -1,59 +1,125 @@
 export const DIAMOND_SPONSORS = [
-    {
-        name: "Google for Developers",
-        image: "/images/sponsor-&-partner/google-for-developers.webp",
-    },
-    {
-        name: "Logitech",
-        image: "/images/sponsor-&-partner/logitech.webp",
-    },
-    {
-        name: "MSI",
-        image: "/images/sponsor-&-partner/msi.webp",
-        class: "py-[21px] md:py-[36px] lg:py-[56px]",
-    }
-]
+  {
+    name: "Google for Developers",
+    image: "/images/sponsor-&-partner/google-for-developers.webp",
+    link: "https://developers.google.com/",
+  },
+  {
+    name: "Logitech",
+    image: "/images/sponsor-&-partner/logitech.webp",
+    link: "https://www.logitech.com/id-id",
+  },
+  {
+    name: "MSI",
+    image: "/images/sponsor-&-partner/msi.webp",
+    class: "py-[21px] md:py-[36px] lg:py-[56px]",
+    link: "https://id.msi.com/",
+  },
+];
 
 export const PLATINUM_SPONSORS = [
-    {
-        name: "WPU Course",
-        image: "/images/sponsor-&-partner/wpu-course.webp",
-    },
-    {
-        name: "Kliktron",
-        image: "/images/sponsor-&-partner/kliktron.webp",
-    },
-]
+  {
+    name: "WPU Course",
+    image: "/images/sponsor-&-partner/wpu-course.webp",
+    link: "https://wpucourse.id/",
+  },
+  {
+    name: "Kliktron",
+    image: "/images/sponsor-&-partner/kliktron.webp",
+    link: "https://kliktron.id/",
+  },
+];
 
 export const SILVER_SPONSORS = [
-    {
-        name: "Cuci Wangi",
-        image: "/images/sponsor-&-partner/cuci-wangi.webp",
-    },
-    {
-        name: "Sarae Bakery",
-        image: "/images/sponsor-&-partner/sarae-bakery.webp",
-    },
-    {
-        name: "Lofi Coffee & Space",
-        image: "/images/sponsor-&-partner/lo-fi.webp",
-    },{
-        name: "heviitro",
-        image: "/images/sponsor-&-partner/heviitro.webp",
-    }
-]
+  {
+    name: "Cuci Wangi",
+    image: "/images/sponsor-&-partner/cuci-wangi.webp",
+    link: "https://www.cuciwangi.com/",
+  },
+  {
+    name: "Sarae Bakery",
+    image: "/images/sponsor-&-partner/sarae-bakery.webp",
+    link: "https://www.instagram.com/sarae_bakery/",
+  },
+  {
+    name: "Lofi Coffee & Space",
+    image: "/images/sponsor-&-partner/lo-fi.webp",
+    link: "https://www.instagram.com/lofi.jbr/",
+  },
+  {
+    name: "heviitro",
+    image: "/images/sponsor-&-partner/heviitro.webp",
+    link: "https://www.instagram.com/heviitro.official/",
+  },
+];
 
 export const MEDIA_PARTNERS = [
-    {
-        name: "Radar Bandung",
-        image: "/images/sponsor-&-partner/radar-bandung.webp",
-    },
-    {
-        name: "bradio",
-        image: "/images/sponsor-&-partner/bradio.webp",
-        class: "py-[13px] md:py-4 lg:py-[18px]"
-    },{
-        name: "naramakna.id",
-        image: "/images/sponsor-&-partner/naramakna.webp",
-    }
-]
+  {
+    name: "radar bandung",
+    image: "/images/sponsor-&-partner/radar-bandung.webp",
+    link: "https://www.radarbandung.id/",
+  },
+  {
+    name: "bradio",
+    image: "/images/sponsor-&-partner/bradio.webp",
+    class: "py-[13px] md:py-4 lg:py-[18px]",
+    link: "https://bradio956.fm/",
+  },
+  {
+    name: "naramakna.id",
+    image: "/images/sponsor-&-partner/naramakna.webp",
+    link: "https://naramakna.id/",
+  },
+  {
+    name: "antaranews.com",
+    image: "/images/sponsor-&-partner/antara-news.webp",
+    link: "https://www.antaranews.com/",
+  },
+  {
+    name: "ayobandung.com",
+    image: "/images/sponsor-&-partner/ayo-bandung.webp",
+    link: "https://www.ayobandung.com/",
+  },
+  {
+    name: "bandung 24 jam",
+    image: "/images/sponsor-&-partner/bandung-24-jam.webp",
+    link: "https://www.instagram.com/bandung24jam/",
+  },
+  {
+    name: "berita bandung id",
+    image: "/images/sponsor-&-partner/berita-bandung-id.webp",
+    link: "https://beritabandung.id/",
+  },
+  {
+    name: "dunia coding",
+    image: "/images/sponsor-&-partner/dunia-coding.webp",
+    link: "https://duniacoding.id/",
+  },
+  {
+    name: "folkjawabarat",
+    image: "/images/sponsor-&-partner/folkjawabarat.webp",
+    link: "https://duniacoding.id/",
+  },
+  {
+    name: "IBBC",
+    image: "/images/sponsor-&-partner/ibbc.webp",
+    class: "!py-1 !px-2 !md:py-2 !lg:py-2",
+    link: "https://www.instagram.com/infobdgbaratcimahi/",
+  },
+  {
+    name: "infobdg.com",
+    image: "/images/sponsor-&-partner/infobdg.webp",
+    link: "https://www.infobdg.com/v2/",
+  },
+  {
+    name: "jabarekspres.com",
+    image: "/images/sponsor-&-partner/jabar-ekpress.webp",
+    class: "!py-1 !px-1 !md:py-1 !lg:py-1",
+    link: "https://jabarekspres.com/",
+  },
+  {
+    name: "jabarnews.com",
+    image: "/images/sponsor-&-partner/jabarnews.webp",
+    link: "https://www.jabarnews.com/",
+  },
+];
